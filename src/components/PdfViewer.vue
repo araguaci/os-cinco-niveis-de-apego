8178063d57cbb5cdadbf8df78130b0dd9bf60ab6
araguaci <araguaci@gmail.com>
2023-06-23 15:11:51 -0300

    Os cinco níveis de apego, Don Miguel Ruiz Jr.

---
+++ src/components/PdfViewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.openPdf()
+  },
+  methods: {
+    openPdf() {
+      const file = this.$route.params.file
+      const pdfUrl = '/os-cinco-niveis-de-apego' + file + '.pdf'
+      // window.open(pdfUrl, '_blank')
+    }
+  }
+}
+</script>
